13be144913a1bf047b676fe6a6370f2ac2df8b8f
jayrajthakor <jayrajthakor25@gmail.com>
2022-03-27 20:36:33 -0400

    change react app base url from localhost to heroku remote server

--- src/tests/user-list.test.js
+++ src/tests/user-list.test.js
@@ -3,9 +3,8 @@ import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import {findAllUsers} from "../services/users-service";
 import axios from "axios";
-import {createUser} from "./services";
 
-jest.mock('axios');
+// jest.mock('axios');
 
 const MOCKED_USERS = [
   {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
@@ -14,9 +13,9 @@ const MOCKED_USERS = [
 
 test('user list renders static user array', () => {
   render(
-    <HashRouter>
-      <UserList users={MOCKED_USERS}/>
-    </HashRouter>);
+      <HashRouter>
+        <UserList users={MOCKED_USERS}/>
+      </HashRouter>);
   const linkElement = screen.getByText(/ellen_ripley/i);
   expect(linkElement).toBeInTheDocument();
 });
@@ -24,24 +23,27 @@ test('user list renders static user array', () => {
 test('user list renders async', async () => {
   const users = await findAllUsers();
   render(
-    <HashRouter>
-      <UserList users={users}/>
-    </HashRouter>);
-  const linkElement = screen.getByText(/alice/i);
+      <HashRouter>
+        <UserList users={users}/>
+      </HashRouter>);
+  const linkElement = screen.getByText(/NASA/i);
   expect(linkElement).toBeInTheDocument();
 })
 
+
 test('user list renders mocked', async () => {
-  axios.get.mockImplementation(() =>
-    Promise.resolve({ data: {users: MOCKED_USERS} }));
+  const mock = jest.spyOn(axios, 'get');
+  mock.mockImplementation(() =>
+      Promise.resolve({ data: {users: MOCKED_USERS} }));
 
   const response = await findAllUsers();
+  mock.mockRestore();  // restore original implementation
   const users = response.users;
 
   render(
-    <HashRouter>
-      <UserList users={users}/>
-    </HashRouter>);
+      <HashRouter>
+        <UserList users={users}/>
+      </HashRouter>);
 
   const user = screen.getByText(/ellen_ripley/i);
   expect(user).toBeInTheDocument();
