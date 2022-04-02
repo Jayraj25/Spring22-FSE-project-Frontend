d14a9740e3f4b29024e8bdc6ffefd8145f23321f
jayrajthakor <jayrajthakor25@gmail.com>
2022-04-02 17:18:37 -0400

    added dislikes stats and made changes to accomodate dislikes feature

--- src/components/tuits/tuitStats.test.js
+++ src/components/tuits/tuitStats.test.js
@@ -5,7 +5,7 @@ test('stats render correctly', () => {
     let stats = {
         likes: 123,
         replies: 234,
-        retuits: 345
+        retuits: 345,
     }
 
     const likeTuit = () => {
@@ -32,8 +32,7 @@ test('stats render correctly', () => {
     const likesCounter = root.findByProps({className: 'ttr-like-stats'});
     const retuitsCounter = root.findByProps({className: 'ttr-stats-retuits'})
     const repliesCounter = root.findByProps({className: 'ttr-stats-replies'})
-    const likeTuitButton = root.findByProps(
-        {className: 'ttr-like-tuit-click'})
+    const likeTuitButton = root.findByProps({className: 'ttr-like-tuit-click'})
 
     let likesText = likesCounter.children[0];
     const repliesText = repliesCounter.children[0];
@@ -45,4 +44,54 @@ test('stats render correctly', () => {
     act(() => {likeTuitButton.props.onClick()})
     likesText = likesCounter.children[0];
     expect(likesText).toBe('124');
+});
+
+test("stats render correctly for dislikes", () => {
+    let stats = {
+        likes: 123,
+        replies: 234,
+        retuits: 345,
+        dislikes: 456,
+    }
+
+    const dislikeTuit = () => {
+        act(() => {
+            stats.dislikes++;
+            tuitStats.update(
+                <TuitStats
+                    tuit={{stats: stats}}
+                    dislikeTuit={() => {}}
+                />)
+        })
+    }
+
+    let tuitStats
+    act(() => {
+        tuitStats = create(
+            <TuitStats
+                dislikeTuit={dislikeTuit}
+                tuit={{stats: stats}}/>
+        );
+    })
+
+    const root = tuitStats.root;
+    const likesCounter = root.findByProps({className: 'ttr-like-stats'});
+    const retuitsCounter = root.findByProps({className: 'ttr-stats-retuits'})
+    const repliesCounter = root.findByProps({className: 'ttr-stats-replies'})
+    const dislikesCounter = root.findByProps({className: 'ttr-dislike-stats'});
+    const dislikeTuitButton = root.findByProps({className: 'ttr-dislike-tuit-click'})
+
+    let likesText = likesCounter.children[0];
+    const repliesText = repliesCounter.children[0];
+    const retuitsText = retuitsCounter.children[0];
+    expect(likesText).toBe('123');
+    expect(repliesText).toBe('234');
+    expect(retuitsText).toBe('345');
+
+    let dislikesText = dislikesCounter.children[0];
+    expect(dislikesText).toBe('456');
+
+    act(() => {dislikeTuitButton.props.onClick()})
+    dislikesText = dislikesCounter.children[0];
+    expect(dislikesText).toBe('457');
 });
