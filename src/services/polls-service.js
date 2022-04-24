4d430d6f4c89a02037ee1ad5d83c06eda2aeee56
vivek <vivek>
2022-04-24 19:57:44 -0400

    Service for My created polls tab in profile

--- src/services/polls-service.js
+++ src/services/polls-service.js
@@ -40,6 +40,12 @@ export const closePoll = (uid, pid, poll) =>
     api.put(`${USERS_API}/${uid}/close/polls/${pid}`, poll)
         .then(response => response.data);
 
+export const findPollsByCreatedUser = (uid) =>
+    api.get(`${USERS_API}/${uid}/pollscreated`).then(response => response.data);
+
 export const findAllPollsResponseById = (pid) =>
     api.get(`${BASE_URL}/api/responses/polls/${pid}`)
         .then(response => response.data);
+
+export const findPollsRespondedByUser = (uid) =>
+    api.get(`${USERS_API}/${uid}/pollsresponded`).then(response => response.data);
